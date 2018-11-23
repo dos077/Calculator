831283fd8658ae13fd58c13cd8fca7928e2686f2
dos077 <zeming@dreamofsky.com>
2018-11-23 12:50:28 -0500

    completed data entry, operate, calculate, and screen clear functions.

--- js/cal.js
+++ js/cal.js
@@ -1,8 +1,12 @@
+var displayLimit = 10;
+
 function enterDigit(e) {
     digit = e.getAttribute('id');
     var span;
-    var display = document.querySelector('.display');;
-    var displayLimit = 10;
+    var display = checkDisplay('data');
+    if(display.querySelector('.answer')) {
+        displayClear();
+    }
     var numDigit = document.querySelectorAll('div.display > span.num').length;
     if( document.querySelector('#zero') && digit!=0 ){
         display.innerHTML = '';
@@ -42,8 +46,9 @@ function spaceDisplay() {
     }
 }
 function enterDec() {
-    var display = document.querySelector('.display');
+    var display = checkDisplay('data');
     var decSpan;
+    var numDigit = display.querySelectorAll('.num').length;
     if( document.querySelector('#zero') ) {
         display.innerHTML = '';
         var zeroSpan = document.createElement('span');
@@ -56,7 +61,7 @@ function enterDec() {
         decSpan.style.order = 1;
         display.appendChild(zeroSpan);
         display.appendChild(decSpan);
-    } else if ( !(display.querySelector('.dec')) ) {
+    } else if ( !(display.querySelector('.dec')) && numDigit < displayLimit ) {
         var intDigit = display.querySelectorAll('.int').length;
         decSpan = document.createElement('span');
         decSpan.innerHTML = '.';
@@ -66,7 +71,7 @@ function enterDec() {
     }
 }
 function enterSign() {
-    var display = document.querySelector('.display');
+    var display= checkDisplay('data');
     var numDigit = document.querySelectorAll('.num').length;
     if (numDigit>0) {
         var signSpan = display.querySelector('.sign');
@@ -81,23 +86,39 @@ function enterSign() {
         }
     }
 }
-function enterClear() {
+function displayClear() {
     var display = document.querySelector('.display');
-    var numDigit = display.querySelectorAll('.num').length;
-    if (numDigit > 0) {
-        var zero = document.createElement('span');
-        zero.id = 'zero';
-        zero.innerHTML = '0';
-        display.innerHTML = '';
-        display.appendChild(zero);
+    if( display.querySelector('#zero') ) {
+        var currentOp = document.querySelector('#current');
+        if(currentOp) {currentOp.parentNode.removeChild(currentOp);}
+    } else {
+        var numDigit = display.querySelectorAll('.num').length;
+        var lastAnswer = document.querySelector('.lastAn');
+        if(lastAnswer) {
+            lastAnswer.classList.remove('lastAn');
+        }
+        if (numDigit > 0) {
+            var zero = document.createElement('span');
+            zero.id = 'zero';
+            zero.innerHTML = '0';
+            display.innerHTML = '';
+            display.appendChild(zero);
+        } else if (document.querySelector('#error')) {
+            var zero = document.createElement('span');
+            zero.id = 'zero';
+            zero.innerHTML = '0';
+            display.innerHTML = '';
+            display.appendChild(zero);
+        }
     }
+    
 }
 function enterBs() {
     var display = document.querySelector('.display');
     var numDigit = display.querySelectorAll('.num').length;
     if (numDigit > 0) {
         if (numDigit == 1) {
-            enterClear();
+            displayClear();
         } else {
             var digits = display.querySelectorAll('.num, .dec');
             display.removeChild(digits[digits.length-1]);
@@ -118,4 +139,146 @@ function readDisplay() {
         }
         return display.textContent;
     }
+}
+function operate(e) {
+    var memory = document.querySelector('.memory');
+    var currentOp = memory.querySelector('#current');
+    if (currentOp) {
+        if(currentOp.querySelector('.op')) {
+            if(calculate()) {
+                operate(e);
+            } else {
+                return false;
+            }
+        } else {
+            var opSpan = document.createElement('span');
+            opSpan.classList.add('op');
+            opSpan.id = e.id;
+            opSpan.innerHTML = e.textContent;
+            currentOp.appendChild(opSpan);
+            enterClear();
+        }
+    } else if(!(document.querySelector('#zero')) && !(document.querySelector('#error')) ) {
+        var lastAnswer = document.querySelector('.lastAn');
+        if(lastAnswer) {
+            lastAnswer.classList.remove('lastAn');
+        }
+
+        currentOp = document.createElement('div');
+        currentOp.classList.add('workOp');
+        currentOp.id = 'current';
+        numSpan = document.createElement('span');
+        numSpan.classList.add('num');
+        numSpan.innerHTML = readDisplay();
+        opSpan = document.createElement('span');
+        opSpan.classList.add('op');
+        opSpan.id = e.id;
+        opSpan.innerHTML = e.textContent;
+        currentOp.appendChild(numSpan);
+        currentOp.appendChild(opSpan);
+        memory.appendChild(currentOp);
+        displayClear();
+    }
+}
+function displayAnswer(answer) {
+    var display = document.querySelector('.display');
+    answer = Number(answer);
+    if ( answer > Math.pow(10, displayLimit) ) {
+        displayError('Too Many Digits');
+    } else if ( answer < ( -1 * Math.pow(10, displayLimit) ) ) {
+        displayError('Too Many Digits');
+    } else {
+        var anSpan = document.createElement('span');
+        anSpan.classList.add('answer', 'num');
+        answer = answer.toString();
+        
+        var spanLimit = displayLimit;
+        if (answer.search('-') >= 0) {
+            spanLimit++;
+        }
+        if (answer.search('.')) {
+            spanLimit++;
+        }
+        if (answer.length < spanLimit) {
+            anSpan.innerHTML = answer;
+        } else {
+            anSpan.innerHTML = answer.slice(0, (spanLimit));
+        }
+        display.innerHTML = '';
+        display.appendChild(anSpan);
+    }  
+}
+function displayError(e) {
+    var display = document.querySelector('.display');
+    var errorSpan = document.createElement('span');
+    errorSpan.setAttribute('id', 'error');
+    errorSpan.innerHTML = e;
+    display.innerHTML = '';
+    display.appendChild(errorSpan);
+}
+function calculate() {
+    var display = checkDisplay('calculate');
+    var currentOp = document.querySelector('#current');
+    if (currentOp) {
+        if (display.querySelector('#zero')) {
+            return false;
+        } else {
+            var lastAnswer = document.querySelector('.lastAn');
+            if(lastAnswer) {
+                lastAnswer.classList.remove('lastAn');
+            }
+
+            var op = currentOp.querySelector('.op');
+            var input1 = Number(currentOp.querySelector('.num').textContent);
+            var input2 = Number(readDisplay());
+            var eqSpan = document.createElement('span');
+            eqSpan.classList.add('eq');
+            eqSpan.innerHTML = '=';
+            var num2Span = document.createElement('span');
+            num2Span.classList.add('num');
+            num2Span.innerHTML = input2;
+            var anSpan = document.createElement('span');
+            anSpan.classList.add('an');
+            anSpan.classList.add('lastAn');
+            if (op.id == 'ad') {
+                var answer = input1 + input2;
+                anSpan.innerHTML = answer;
+                displayAnswer(answer);
+            }
+            if (op.id == 'sb') {
+                var answer = input1 - input2;
+                anSpan.innerHTML = answer;
+                displayAnswer(answer);
+            }
+            if(op.id == 'ml') {
+                var answer = input1 * input2;
+                anSpan.innerHTML = answer;
+                displayAnswer(answer);
+            }
+            if(op.id == 'dv') {
+                if(input2==0) {
+                    displayClear();
+                    document.querySelector('.memory').removeChild(currentOp);
+                    currentOp = null;
+                    displayError('Indeterminate');
+                }
+                else {
+                    var answer = input1 / input2;
+                    anSpan.innerHTML = answer;
+                    displayAnswer(answer);
+                }
+            }
+            currentOp.appendChild(num2Span);
+            currentOp.appendChild(eqSpan);
+            currentOp.appendChild(anSpan);
+            currentOp.removeAttribute('id');
+        }
+    }
+}
+function checkDisplay(source) {
+    var display = document.querySelector('.display');
+    if( display.querySelector('#error') && source != 'clear' ) {
+        display = null;
+    }
+    return display;
 }
